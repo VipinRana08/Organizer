9d280765144f9726ded1ac9c26d8eb1d77ca99ca
Vipin Singh Rana <vipinrana08.vr@gmail.com>
2024-08-19 14:15:09 +0530

    add categories for files

---
+++ utility/utility.js
@@ -0,0 +1,7 @@
+utility={
+    media:["mp4","mkv"],
+    archives:["zip","7z",'rar','tar','iso'],
+    documents:['docx','doc','pdf','xlsx','txt'],
+    app:['exe','dmg','pkg'],
+    picture:['png','jpg']
+}
