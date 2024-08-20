10f3f490393a1362f8f178153d27d5e10ec0ddf8
Vipin Singh Rana <vipinrana08.vr@gmail.com>
2024-08-20 23:05:09 +0530

    add help function

---
+++ commands/help.js
@@ -0,0 +1,8 @@
+exports.helpFunction = function (){
+    console.log(` List of all commands :
+                        1. help
+                        2. tree "Path of the directory"
+                        3. organize "Path of the directory"                
+        `
+    );
+}
