8fec5ec140fde22506dd5619194546111cfcf392
Vipin Singh Rana <vipinrana08.vr@gmail.com>
2024-08-20 23:03:48 +0530

    correct utility file

--- utility/utility.js
+++ utility/utility.js
@@ -1,7 +1,9 @@
-utility={
+const types={
     media:["mp4","mkv"],
     archives:["zip","7z",'rar','tar','iso'],
     documents:['docx','doc','pdf','xlsx','txt'],
     app:['exe','dmg','pkg'],
     picture:['png','jpg']
 }
+
+module.export = { types }
